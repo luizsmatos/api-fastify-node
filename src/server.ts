98fca1d762476091cae0c37bca6bf1ebdf275e1d
Luiz Gustavo <gusstavopdm@gmail.com>
2023-02-10 07:36:14 -0300

    feat(server): add transactionsRoutes plugin

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import fastify from 'fastify'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify({ logger: true })
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
 
 app
   .listen({
