26ef9658a19b5a3cc6c5db25fce8bf49a155e3d2
Luiz Gustavo <gusstavopdm@gmail.com>
2023-02-21 11:17:35 -0300

    test: transaction listing

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -18,4 +18,24 @@ describe('Transactions routes', () => {
 
     expect(response.status).toBe(201)
   })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({ title: 'New transaction', amount: 5000, type: 'credit' })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    expect(listTransactionsResponse.status).toBe(200)
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
+  })
 })
